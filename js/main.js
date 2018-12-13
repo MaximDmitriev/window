da23812afb94eb7620dbd234c9cb930336a64cc9
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2018-12-13 14:02:13 +0300

    works picture

--- js/main.js
+++ js/main.js
@@ -76,9 +76,9 @@ window.addEventListener('DOMContentLoaded', () => {
         decorationTabName = decoration.querySelectorAll(".decoration_item"),
         decorationLink = decoration.querySelectorAll("a");
 
-        console.log(decorationTabContent);
-        console.log(decorationTabName);
-        console.log(decorationLink);
+        // console.log(decorationTabContent);
+        // console.log(decorationTabName);
+        // console.log(decorationLink);
         
         function hideDecoration(count){
             for(let i = count; i < decorationTabContent.length; i++){
@@ -102,5 +102,47 @@ window.addEventListener('DOMContentLoaded', () => {
     
             });
         });
+
+    //works
+
+    let works = document.querySelector(".works"),
+        imgPreview = works.querySelectorAll(".row>div"),
+        overlayWorks = document.createElement('div'),
+        bigImageWorks = document.createElement('div');
+
+        
+    overlayWorks.classList.add("overlayworks");
+    overlayWorks.classList.add("animated");
+    overlayWorks.classList.add("fadeIn");
+
+    bigImageWorks.classList.add("bigImageWorks");
+    
+
+    imgPreview.forEach((item) =>{
+        item.querySelector("a").addEventListener('click', (event) =>{
+            event.preventDefault();
+        });
+        item.querySelector("a").lastChild.addEventListener('click', () =>{
+            document.body.appendChild(overlayWorks);
+            document.getElementsByTagName("html")[0].style.overflow = "hidden";
+            
+            let url = item.querySelector("a").getAttribute("href");
+
+            overlayWorks.appendChild(bigImageWorks);
+            bigImageWorks.style.backgroundImage = `url(${url})`;
+
+            overlayWorks.addEventListener('click', function() {
+                overlayWorks.classList.add("fadeOut");
+                setTimeout(() => {
+                    overlayWorks.classList.remove("fadeOut");
+
+                    document.body.removeChild(document.body.lastChild);
+                    document.getElementsByTagName("html")[0].style.overflow = "";
+                }, 1000);
+            });
+        });
+    });
+
+
 
 });
